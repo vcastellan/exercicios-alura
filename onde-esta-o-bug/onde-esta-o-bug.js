e0cb53eb47dc4cfc78d0366a1e01fd3422e46590
vcastellan <victor_castellan@hotmail.com>
2023-10-17 16:09:01 -0300

    adicionando exercicio aula 01 item 08

---
+++ onde-esta-o-bug/onde-esta-o-bug.js
@@ -0,0 +1,22 @@
+//Luis é apaixonado por tecnologia e está dando seus primeiros passos no mundo da programação. Para praticar lógica de programação, ele decidiu desenvolver um sistema de login de usuário único em que é necessário apenas informar a senha para ver o conteúdo do restante do projeto.
+//No entanto, ao testar o seu projeto, percebeu que não importava a senha passada para o sistema, o acesso sempre era permitido. Sabendo que você também está estudando lógica de programação, Luis pediu sua ajuda para que vocês consigam encontrar o bug juntos. Para isso, ele disponibilizou o código que escreveu:
+
+
+
+let senhaDoSistema = "senhaTeste";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha = senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}
+
+
+
+
+//Qual alteração a seguir você poderia sugerir ao Luis para que o seu sistema funcione como imaginado?
+
+// A) O código apresenta um bug por conta dos nomes das variáveis, que são muito parecidos e parecem se tratar da mesma coisa.
+// B) O código está funcionando perfeitamente, e o problema é o navegador de internet. Para o sistema funcionar, basta mudar o navegador.
+// C) O código apresenta um bug dentro da estrutura condicional, pois o operador de comparação não está sendo utilizado corretamente.
+// D) O código apresenta um bug porque as funções alert e prompt estão invertidas.
